fb75849c51cb919ae7c8cd9fa69a271c07fa4307
MajinBluee <43613100+MajinBluee@users.noreply.github.com>
2018-12-10 15:13:59 +0200

    Create fart.js

---
+++ commands/fart.js
@@ -0,0 +1,11 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+message.channel.send('EWWWWWW, Please dont fart here.')
+
+  }
+
+module.exports.help = {
+  name:"fart"
+}
